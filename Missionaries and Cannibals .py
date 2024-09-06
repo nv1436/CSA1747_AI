250cce7478204dbb0eee323b13a1191d8d75a9bf
nv1436 <129158926+nv1436@users.noreply.github.com>
2024-09-06 13:27:55 +0530

    Missionaries and Cannibals .py

--- Missionaries and Cannibals .py
+++ Missionaries and Cannibals .py
@@ -1,89 +1,95 @@
-from collections import deque
+#Python program to illustrate Missionaries & cannibals Problem 
+#This code is contributed by Sunit Mal 
+print("\n") 
+print("\tGame Start\nNow the task is to move all of them to right side of the river") 
+print("rules:\n1. The boat can carry at most two people\n2. If cannibals num greater than missionaries then the cannibals would eat the missionaries\n3. The boat cannot cross the river by itself with no people on board") 
+lM = 3		 #lM = Left side Missionaries number 
+lC = 3		 #lC = Laft side Cannibals number 
+rM=0		 #rM = Right side Missionaries number 
+rC=0		 #rC = Right side cannibals number 
+userM = 0	 #userM = User input for number of missionaries for right to left side travel 
+userC = 0	 #userC = User input for number of cannibals for right to left travel 
+k = 0
+print("\nM M M C C C |	 --- | \n") 
+try: 
+	while(True): 
+		while(True): 
+			print("Left side -> right side river travel") 
+			#uM = user input for number of missionaries for left to right travel 
+			#uC = user input for number of cannibals for left to right travel 
+			uM = int(input("Enter number of Missionaries travel => "))	 
+			uC = int(input("Enter number of Cannibals travel => ")) 
 
-class State:
-    def __init__(self, m_left, c_left, m_right, c_right, boat):
-        self.m_left = m_left
-        self.c_left = c_left
-        self.m_right = m_right
-        self.c_right = c_right
-        self.boat = boat
+			if((uM==0)and(uC==0)): 
+				print("Empty travel not possible") 
+				print("Re-enter : ") 
+			elif(((uM+uC) <= 6)and((lM-uM)>=0)and((lC-uC)>=0)): 
+				break
+			else: 
+				print("Wrong input re-enter : ") 
+		lM = (lM-uM) 
+		lC = (lC-uC) 
+		rM += uM 
+		rC += uC 
 
-    def __repr__(self):
-        return (f"State(M_left={self.m_left}, C_left={self.c_left}, "
-                f"M_right={self.m_right}, C_right={self.c_right}, Boat={self.boat})")
+		print("\n") 
+		for i in range(0,lM): 
+			print("M ",end="") 
+		for i in range(0,lC): 
+			print("C ",end="") 
+		print("| --> | ",end="") 
+		for i in range(0,rM): 
+			print("M ",end="") 
+		for i in range(0,rC): 
+			print("C ",end="") 
+		print("\n") 
 
-    def is_valid(self):
-        # Check if the state is valid
-        if (self.m_left < 0 or self.m_left > 3 or
-            self.c_left < 0 or self.c_left > 3 or
-            self.m_right < 0 or self.m_right > 3 or
-            self.c_right < 0 or self.c_right > 3 or
-            (self.m_left < self.c_left and self.m_left > 0) or
-            (self.m_right < self.c_right and self.m_right > 0)):
-            return False
-        return True
+		k +=1
 
-    def is_goal(self):
-        # Check if the goal state is reached
-        return self.m_left == 0 and self.c_left == 0
+		if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))): 
+			print("Cannibals eat missionaries:\nYou lost the game") 
 
-    def get_successors(self):
-        successors = []
-        if self.boat == 'left':
-            for m in range(3):
-                for c in range(3):
-                    if (m + c >= 1 and m + c <= 2 and
-                        m <= self.m_left and c <= self.c_left):
-                        new_state = State(self.m_left - m, self.c_left - c,
-                                          self.m_right + m, self.c_right + c, 'right')
-                        if new_state.is_valid():
-                            successors.append(new_state)
-        else:
-            for m in range(3):
-                for c in range(3):
-                    if (m + c >= 1 and m + c <= 2 and
-                        m <= self.m_right and c <= self.c_right):
-                        new_state = State(self.m_left + m, self.c_left + c,
-                                          self.m_right - m, self.c_right - c, 'left')
-                        if new_state.is_valid():
-                            successors.append(new_state)
-        return successors
+			break
 
-def bfs():
-    start = State(3, 3, 0, 0, 'left')
-    goal = State(0, 0, 3, 3, 'right')
-    
-    queue = deque([start])
-    visited = set()
-    parent_map = {start: None}
+		if((rM+rC) == 6): 
+			print("You won the game : \n\tCongrats") 
+			print("Total attempt") 
+			print(k) 
+			break
+		while(True): 
+			print("Right side -> Left side river travel") 
+			userM = int(input("Enter number of Missionaries travel => ")) 
+			userC = int(input("Enter number of Cannibals travel => ")) 
+			
+			if((userM==0)and(userC==0)): 
+					print("Empty travel not possible") 
+					print("Re-enter : ") 
+			elif(((userM+userC) <= 2)and((rM-userM)>=0)and((rC-userC)>=0)): 
+				break
+			else: 
+				print("Wrong input re-enter : ") 
+		lM += userM 
+		lC += userC 
+		rM -= userM 
+		rC -= userC 
 
-    while queue:
-        current = queue.popleft()
-        
-        if current.is_goal():
-            path = []
-            while current is not None:
-                path.append(current)
-                current = parent_map[current]
-            return path[::-1]
-        
-        visited.add(current)
-        
-        for next_state in current.get_successors():
-            if next_state not in visited and next_state not in queue:
-                queue.append(next_state)
-                parent_map[next_state] = current
-                
-    return None
+		k +=1
+		print("\n") 
+		for i in range(0,lM): 
+			print("M ",end="") 
+		for i in range(0,lC): 
+			print("C ",end="") 
+		print("| <-- | ",end="") 
+		for i in range(0,rM): 
+			print("M ",end="") 
+		for i in range(0,rC): 
+			print("C ",end="") 
+		print("\n") 
 
-def print_solution(path):
-    if path:
-        for step in path:
-            print(step)
-            print()
-    else:
-        print("No solution found.")
+	
 
-# Example Usage
-solution_path = bfs()
-print_solution(solution_path)
+		if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))): 
+			print("Cannibals eat missionaries:\nYou lost the game") 
+			break
+except EOFError as e: 
+	print("\nInvalid input please retry !!")
